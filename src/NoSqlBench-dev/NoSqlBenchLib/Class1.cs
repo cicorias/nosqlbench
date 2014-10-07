327c2b383911ee8d8f2d6bf01c9a9aa91f06f395
Shawn Cicoria <shawn@cicoria.com>
2014-10-07 12:31:22 -0400

    initial

---
+++ src/NoSqlBench-dev/NoSqlBenchLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoSqlBenchLib
+{
+    public class Class1
+    {
+    }
+}
