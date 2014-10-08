d549b3d5c37945d3ce0fdac20ca5954718532aff
Shawn Cicoria <shawn@cicoria.com>
2014-10-08 13:20:30 -0400

    adding own as self host option for client

---
+++ src/NoSqlBenchClient/ClientHttpConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace NoSqlBenchClient
+{
+    public class ClientHttpConfiguration : HttpConfiguration
+    {
+        public ClientHttpConfiguration()
+        {
+            ConfigureRoutes();
+            ConfigureJsonSerialization();
+
+        }
+
+        private void ConfigureRoutes()
+        {
+            var tt = base.VirtualPathRoot;
+
+            Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        private void ConfigureJsonSerialization()
+        {
+#if DEBUG
+            var jsonSettings = Formatters.JsonFormatter.SerializerSettings;
+            jsonSettings.Formatting = Formatting.Indented;
+#endif
+        }
+    }
+}
